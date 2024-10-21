4df61b91cf99a90a65809fec41e852b7939fe61d
Kirill <kirill98s@bk.ru>
2024-10-21 22:49:40 +0300

    ex deps init

--- cmd/sandbox-command-first/main.go
+++ cmd/sandbox-command-first/main.go
@@ -3,5 +3,10 @@ package main
 import "fmt"
 
 func main() {
+
 	fmt.Println("start sandbox-command-first")
 }
+
+// func run() error{
+
+// }
--- internal/server/deps.go
+++ internal/server/deps.go
@@ -1,1 +1,10 @@
 package server
+
+import (
+	"log"
+	"sandbox-service/configs"
+)
+
+func ExternalDeps(cfg configs.Config) {
+	log.Println(cfg)
+}
